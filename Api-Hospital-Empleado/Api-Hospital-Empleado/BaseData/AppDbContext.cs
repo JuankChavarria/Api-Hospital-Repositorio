e4c23e77425ea56dce6a3d5c4521ae0122e5ada8
Juan Carlos Nuñez Chavarria <134462352+JuankChavarria@users.noreply.github.com>
2024-12-13 10:27:53 -0600

    DbContect listo para Asintecia

---
+++ Api-Hospital-Empleado/Api-Hospital-Empleado/BaseData/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Api_Hospital_Empleado.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_Hospital_Empleado.BaseData
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Asistencia> Asistencias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+                modelBuilder.Entity<Asistencia>()
+                    .HasKey(a => a.IdAsistencia);
+            }
+        }
+    }
+
